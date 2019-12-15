a2939928f68468bba435002e72e444fa21a46178
dkurata38 <d.kurata.38@gmail.com>
2019-12-15 18:12:29 +0900

    ナビゲーションバーを実装

--- src/App.vue
+++ src/App.vue
@@ -35,27 +35,43 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
+      <v-btn icon
+        @click.stop="drawer = !drawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
     </v-app-bar>
-
+    <v-navigation-drawer
+            v-model="drawer" temporary absolute>
+      <v-list>
+        <v-list-item v-for="(navigationItem, i) in navigationItems"
+          :key="i" link>
+          <v-list-item-icon>
+            <v-icon v-text="navigationItem.icon">
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <router-link v-bind:to="navigationItem.route" v-text="navigationItem.title">
+            </router-link>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default Vue.extend({
-  name: 'App',
-
-  components: {
-    HelloWorld,
+<script type="application/javascript">
+export default {
+  data() {
+    return {
+      drawer: false,
+      navigationItems: [
+        { title: 'HOME', route: '/', icon: 'mdi-home' },
+        { title: 'ABOUT', route: '/about', icon: 'mdi-help-circle' },
+      ],
+    };
   },
-
-  data: () => ({
-    //
-  }),
-});
+};
 </script>
